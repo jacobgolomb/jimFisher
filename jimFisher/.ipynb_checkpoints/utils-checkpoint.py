4bdc5ef96203896d130e252dbd30b9f88970ea42
Jacob Golomb <jgolomb@caltech.edu>
2024-06-27 14:00:43 -0700

    lots of edits

--- jimFisher/.ipynb_checkpoints/utils-checkpoint.py
+++ jimFisher/.ipynb_checkpoints/utils-checkpoint.py
@@ -14,45 +14,93 @@ ASD_FILES = {"CE": f"{sensitivity_path}/LIGO-P1600143-v18-CE-ASD.txt",
 IFO_GEOMETRIC_KEYS = ['latitude', 'longitude',
                     'elevation', 'xarm_azimuth', 'yarm_azimuth',
                     'xarm_tilt', 'yarm_tilt']
-DEFAULT_FISHER_PARAMS = ['log_chirp_mass', 'mass_ratio', 's1_z', 's2_z', 'snr', 'cos_iota']
-WF_FUNCS = {"IMRPhenomD": jimgw.single_event.waveform.RippleIMRPhenomD}
+DEFAULT_FISHER_PARAMS = ['log_chirp_mass', 'mass_ratio', 's1_z', 's2_z', 'log_luminosity_distance', 'cos_iota']
+WF_FUNCS = {"IMRPhenomD": jimgw.single_event.waveform.RippleIMRPhenomD,
+           "IMRPhenomPv2": jimgw.single_event.waveform.RippleIMRPhenomPv2}
 
 BOUNDS = {'mass_ratio': [0,1],
          'cos_iota': [-1, 1],
          's1_z': [-1, 1],
           's2_z': [-1,1],
-          'snr': [0, np.inf]
+          'snr': [0, np.inf],
+          'luminosity_distance': [0, 100000]
+
          }
+
+def bound_theta(parameters):
+    for bounded_param in BOUNDS:
+        for prefix in inverse_operations:
+            if prefix+bounded_param in parameters:
+                bounded = jnp.clip(inverse_operations[prefix](parameters.pop(prefix+bounded_param)),
+                                                     BOUNDS[bounded_param][0] + 1e-6, BOUNDS[bounded_param][1] - 1e-6
+                                  )
+                parameters[prefix+bounded_param] = operations[prefix](bounded)
+    return parameters
+
 LABELS = {'chirp_mass': r"$\mathcal{M}_c$",
          'mass_ratio': r"$q$",
          'luminosity_distance': r"$D_L$",
          'dec': r"$\delta$",
          'ra': r"$\alpha$",
          's1_z': r"$s_{1z}$",
-          's2_z': r"$s_{2z}$",
-          'cos_iota': r"$\cos\iota$",
+         's2_z': r"$s_{2z}$",
+         'cos_iota': r"$\cos\iota$",
          'snr': r"$\rho$",
          'mass_1': r"$m_1$",
-         'mass_2': r"$m_2$"}
+         'mass_2': r"$m_2$",
+         'chi_eff': r"$\chi_{\rm eff}$"}
 
+K = 35
+inverse_operations = {"log_": jnp.exp,
+                     "sin_": jnp.arcsin,
+                     "cos_": lambda x: jnp.arccos(jnp.clip(x, -1.0 + 1e-6, 1.0 - 1e-6)),
+                      "logit_": jax.scipy.special.expit,
+             "onelogit_":lambda f: jnp.nan_to_num((jnp.exp(f/K) - 1)/(1+jnp.exp(f/K)), nan=jnp.inf)
+                     }
+
+operations = {"log_": jnp.log,
+                     "sin_": jnp.sin,
+                     "cos_": jnp.cos,
+             "logit_": jax.scipy.special.logit,
+             "onelogit_": lambda x: K *(jnp.log(1+x) - jnp.log(1-x))}
+
+RIPPLE_KEYS = ['M_c', 'eta', 'd_L', 'iota', 'ra', 'dec', 'phase_c', 'psi', 's1_x', 's1_y', 's1_z', 's2_x', 's2_y', 's2_z', 't_c', 'gmst', 'epoch']
+RIPPLE_MAP = {'M_c': 'chirp_mass', 'd_L': 'luminosity_distance'} 
+    
 mass_params=['mass_1', 'mass_2', 'component_masses', 'total_mass', 'symmetric_mass_ratio']
 
 def convert_to_ripple_params(params):
-    converted = params.copy()
-    
-    converted["eta"] = params['mass_ratio'] / (1 + params['mass_ratio']) ** 2
-    converted['M_c'] = jnp.exp(params['log_chirp_mass'])
-    converted['iota'] = jnp.arccos(params['cos_iota'])
-    converted['dec'] = jnp.arcsin(params['sin_dec'])
-    converted['d_L'] = jnp.exp(params['log_luminosity_distance'])
+    converted = dict()
+    temp = params.copy()
+    for parameter in params:
+        for prefix in inverse_operations:
+            if parameter.startswith(prefix):
+                untransformed_param = parameter.split(prefix)[1]
+                temp[untransformed_param] = inverse_operations[prefix](params[parameter])
+                for pref in inverse_operations:
+                    if untransformed_param.startswith(pref):
+                        temp[untransformed_param.split(pref)[1]] = inverse_operations[pref](temp[untransformed_param])
+
+    temp["eta"] = temp['mass_ratio'] / (1 + temp['mass_ratio']) ** 2
+
+    for parameter in RIPPLE_KEYS:
+        original_key = RIPPLE_MAP.get(parameter, parameter)
+        if original_key in temp: 
+            converted[parameter] = temp[original_key]
     
     return converted
 
 def convert_to_fisher_params(params, fisher_params):
-    converted = params.copy()
+    converted = dict()
+    
     for fisherkey in fisher_params:
-        if fisherkey.startswith('log_'):
-            converted[fisherkey] = jnp.log(params[fisherkey.split("log_")[1]])
+        for prefix in operations:
+            if fisherkey.startswith(prefix):
+                regular_key = fisherkey.split(prefix)[1]
+                if regular_key in params:
+                    converted[prefix+regular_key] = operations[prefix](params[regular_key])
+        if fisherkey not in converted:
+            converted[fisherkey] = params[fisherkey]
     return converted
 
 def inner(a, b, psd_array, freqs):
@@ -62,21 +110,28 @@ def inner(a, b, psd_array, freqs):
 def optimal_snr(signal, psd, freqs):
     return jnp.sqrt(inner(signal, signal, psd, freqs))
 
-def draw_samples(mu, covariance, names, key, N=1000):
-    samples = jax.random.multivariate_normal(key = key, 
-                                                   mean=mu, 
-                                                   cov=covariance, shape=(N,))
+def draw_samples(mu, fisher, names, key, N=1000):
+    samples_unit = jax.random.multivariate_normal(key = key, 
+                                                   mean=jnp.zeros(mu.shape), cov=jnp.eye(len(mu)),shape=(N,))
+    chol = jnp.linalg.cholesky(fisher)
+    scaled_samples = jax.scipy.linalg.solve_triangular(chol.T, samples_unit.T, lower=False).T
+
+    samples = mu + scaled_samples
     samples = {names[i]: samples[:,i] for i in range(len(names))}   
     return samples
 
-def convert_to_physical(samples, rho0=None, dL0=None, generate=['component_masses', 'chi_eff', 'luminosity_distance']):
+def convert_to_physical(samples, rho0=None, dL0=None, generate=['component_masses', 'chi_eff', 'luminosity_distance', 'cos_iota']):
+    if generate is None:
+        generate=[]
     keys = list(samples.keys())
     converted = samples.copy()
-    for key in samples.keys():
-        if key.startswith('log_'):
-            converted[key[4:]] = jnp.exp(samples[key])
-            keys.remove(key)
-            keys.append(key[4:])
+    for prefix in inverse_operations:
+        for key in samples:
+            if key.startswith(prefix):
+                newkey = key.split(prefix)[1]
+                converted[newkey] = inverse_operations[prefix](samples[key])
+                keys.remove(key)
+                keys.append(newkey)
     if ('snr' in keys) and ('luminosity_distance' not in keys):
         if rho0 is not None:
             converted['luminosity_distance'] = rho0 * dL0 / converted['snr']
@@ -84,7 +139,7 @@ def convert_to_physical(samples, rho0=None, dL0=None, generate=['component_masse
     if any(mass_param in generate for mass_param in mass_params):
         converted_masses = dict()
         converted_masses['symmetric_mass_ratio'] = converted['mass_ratio'] / (1 + converted['mass_ratio'])**2
-        converted_masses['total_mass'] = converted_masses['symmetric_mass_ratio']**(-3/5)
+        converted_masses['total_mass'] = converted_masses['symmetric_mass_ratio']**(-3/5) * converted['chirp_mass']
         converted_masses['mass_1'] = converted_masses['total_mass'] / (1 + converted['mass_ratio'])
         converted_masses['mass_2'] = converted_masses['mass_1'] * converted['mass_ratio']
         masskeys = [m for m in mass_params if m in generate]
@@ -96,13 +151,26 @@ def convert_to_physical(samples, rho0=None, dL0=None, generate=['component_masse
             else:
                 converted[key] = converted_masses[key]
                 keys.append(key)
+    if 'chi_eff' in generate:
+        converted['chi_eff'] = (converted['s1_z'] + converted['mass_ratio'] * converted['s2_z']) / (1 + converted['mass_ratio'])
+        keys.append('chi_eff')
+    for parameter in generate:
+        for operation in operations:
+            if (parameter.startswith(operation)):
+                if parameter in converted:
+                    keys.append(parameter)
+                else:
+                    converted[parameter] = operations[operation](converted[parameter.split(operation)[1]])
+                    keys.append(parameter)
         
     return {key: converted[key] for key in keys}
 
 def filter_samples(samples):
+    samples_converted = convert_to_physical(samples,generate=None)
     keep = jnp.ones_like(samples[list(samples.keys())[0]], dtype=bool)
-    for key in BOUNDS.keys():
-        keep &= (samples[key] >= BOUNDS[key][0]) & (samples[key] <= BOUNDS[key][-1])
+    for key in samples_converted.keys():
+        if key in BOUNDS:
+            keep &= (samples_converted[key] >= BOUNDS[key][0]) & (samples_converted[key] <= BOUNDS[key][-1])
     return {key: samples[key][keep] for key in samples}
 
 def compute_distance(v1, v2, g):
--- jimFisher/Fisher.py
+++ jimFisher/Fisher.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[294]:
-
 import jimgw
 from functools import partial
 from astropy.time import Time
@@ -17,7 +15,7 @@ import inspect
 import jimgw.single_event.detector
 import jimgw.single_event.waveform
 from tqdm import trange
-from .utils import filter_samples, convert_to_physical, convert_to_ripple_params, IFO_GEOMETRIC_KEYS, DEFAULT_FISHER_PARAMS, WF_FUNCS, BOUNDS, ASD_FILES, optimal_snr, draw_samples, jimGW_detectors, inner, compute_distance, convert_to_fisher_params
+from .utils import filter_samples, convert_to_physical, convert_to_ripple_params, IFO_GEOMETRIC_KEYS, DEFAULT_FISHER_PARAMS, WF_FUNCS, BOUNDS, ASD_FILES, optimal_snr, draw_samples, jimGW_detectors, inner, compute_distance, convert_to_fisher_params, bound_theta
 
 class FisherSamples(object):
     def __init__(self, name="CE", fmin = 20, fmax = None, 
@@ -52,14 +50,11 @@ class FisherSamples(object):
         self.duration = duration
         
     def set_at_true(self, parameters):
-        self.true_parameters = convert_to_fisher_params(parameters, self.fisher_parameters + ["log_luminosity_distance"])
-        self.true_parameters.update(convert_to_ripple_params(self.true_parameters))
+        self.true_parameters = convert_to_fisher_params(parameters, self.fisher_parameters)
+        self.true_parameters.update(convert_to_ripple_params(parameters))
         self.true_parameters.update({"gmst": self.gmst, "epoch": self.epoch})
-        snr = self.get_snr(self.true_parameters)
-        self.true_parameters['rho0'] = snr
-        self.true_parameters['snr'] = snr
-        self.true_parameters['dL0'] = self.true_parameters['d_L']
-        self.covariance_at_true = self.get_covariance(self.true_parameters)
+
+        self.theta_vec_true = jnp.array([self.true_parameters[key] for key in self.fisher_parameters])
         
     def set_ifo_geometry(self, location):
         if location is None:
@@ -112,7 +107,7 @@ class FisherSamples(object):
         # Placing CE in Hanford so we take the geometric/location parameters of H1 and just replace the PSD with CE's PSD
 
         ifo.psd = self.get_ifo_psd()
-        self.ifo = ifo
+        self.ifo = ifo  
     
     def get_ifo_psd(self):
         
@@ -136,6 +131,7 @@ class FisherSamples(object):
     
     def get_grad_input_args(self, params):
         inputs = []
+        
         for param in self.argnames:
             if param in params:
                 inputs.append(params[param])
@@ -144,30 +140,30 @@ class FisherSamples(object):
         return inputs
     
     def set_argument_orders(self):
-        argnames = list(inspect.signature(self._get_scaled_signal_detector).parameters.keys())
+        argnames = list(inspect.signature(self._get_signal_detector).parameters.keys())
         fisher_argnums = [ii for ii, param in enumerate(argnames) if param in self.fisher_parameters]
         self.argnames = argnames
         self.fisher_argnums = fisher_argnums
         self.fixed_argnames = [arg for ii, arg in enumerate(argnames) if ii not in fisher_argnums]
     
-    def _get_signal_detector(self, log_chirp_mass, mass_ratio, s1_x, s1_y, s1_z, s2_x, s2_y, s2_z, logdL, phase_c, cos_iota,
-                            ra, sin_dec, psi, t_c):
+    #@partial(jax.jit, static_argnums=(0,))    
+    def _get_signal_detector(self, log_chirp_mass, logit_mass_ratio, s1_x, s1_y, s1_z, s2_x, s2_y, s2_z, log_luminosity_distance, phase_c, cos_iota,
+                            ra, dec, psi, t_c):
         
-        params = {"log_chirp_mass": log_chirp_mass, "mass_ratio": mass_ratio, "s1_z": s1_z, "s2_z": s2_z,
-        "log_luminosity_distance": log_luminosity_distance, "phase_c": phase_c, "cos_iota": cos_iota, "ra": ra, "sin_dec": sin_dec, 
+        params = {"log_chirp_mass": log_chirp_mass, "logit_mass_ratio": logit_mass_ratio, "s1_z": s1_z, "s2_z": s2_z,
+        "log_luminosity_distance": log_luminosity_distance, "phase_c": phase_c, "cos_iota":cos_iota, "ra": ra, "dec": dec, 
         "psi": psi, "t_c": t_c, "gmst": self.gmst, "epoch": self.epoch}
         
         converted_parameters = convert_to_ripple_params(params)
-        
         h = self.get_signal_in_detector(converted_parameters)
         return h
     
-    @partial(jax.jit, static_argnums=(0,))
+    #@partial(jax.jit, static_argnums=(0,))
     def _get_scaled_signal_detector(self, log_chirp_mass, mass_ratio, s1_x, s1_y, s1_z, s2_x, s2_y, s2_z, snr, phase_c, cos_iota,
-                            ra, sin_dec, psi, t_c, rho0, dL0):
+                            ra, dec, psi, t_c, rho0, dL0):
         logdL0 = jnp.log(dL0)
         params = {"log_chirp_mass": log_chirp_mass, "mass_ratio": mass_ratio, "s1_z": s1_z, "s2_z": s2_z,
-        "log_luminosity_distance": logdL0, "phase_c": phase_c, "cos_iota": cos_iota, "ra": ra, "sin_dec": sin_dec, 
+        "log_luminosity_distance": logdL0, "phase_c": phase_c, "cos_iota": cos_iota, "ra": ra, "dec": dec, 
         "psi": psi, "t_c": t_c, "gmst": self.gmst, "epoch": self.epoch}
         
         converted_parameters = convert_to_ripple_params(params)
@@ -175,30 +171,28 @@ class FisherSamples(object):
         h = self.get_signal_in_detector(converted_parameters) * snr / rho0
         return h
     
-    @partial(jax.jit, static_argnums=(0,))
+    #@partial(jax.jit, static_argnums=(0,))
     def get_snr(self, parameters):
-        fisher_params = convert_to_fisher_params(parameters, self.fisher_parameters + ["log_luminosity_distance"])
-        converted_parameters = convert_to_ripple_params(fisher_params)
 
+        converted_parameters = convert_to_ripple_params(parameters)
         converted_parameters['gmst'] = parameters.get('gmst', self.gmst)
         converted_parameters['epoch'] = self.epoch
         
         signal = self.get_signal_in_detector(converted_parameters)
         snr = optimal_snr(signal, self.ifo.psd, self.frequency_array)
         return snr
-
-    def get_jacobian(self, parameters = None):
-        if not parameters:
-            parameters = self.parameters
+    
+    #@partial(jax.jit, static_argnums=(0,))
+    def get_jacobian(self, parameters):
         
         inputs = self.get_grad_input_args(parameters)
         
         jac = jnp.array(
-            jax.jacfwd(self._get_scaled_signal_detector, argnums=self.fisher_argnums)(
+            jax.jacfwd(self._get_signal_detector, argnums=self.fisher_argnums)(
             *inputs)
         )
         return jac
-    
+    @partial(jax.jit, static_argnums=(0,))
     def get_fisher(self, parameters=None):
         jacobian = self.get_jacobian(parameters)
         fisher_matrix = jnp.zeros((len(jacobian), len(jacobian)))
@@ -209,46 +203,129 @@ class FisherSamples(object):
                 fisher_matrix = fisher_matrix.at[jj,ii].set(element)    
         return fisher_matrix
     
-    @partial(jax.jit, static_argnums=(0,))
+    #@partial(jax.jit, static_argnums=(0,))
     def get_covariance(self, parameters=None):
         fisher = self.get_fisher(parameters)
         cov = jnp.linalg.inv(fisher)
         return cov
     
-    def theta_vec_to_param_dict(self, theta_vec):
+    def theta_vec_to_param_dict(self, theta_vec, theta_params_true):
         theta_parameters = {key: theta_vec[ii] for ii, key in enumerate(self.fisher_parameters)}
         for key in self.fixed_argnames:
-            theta_parameters[key] = self.true_parameters[key]
+            theta_parameters[key] = theta_params_true[key]
         return theta_parameters
-            
     
-    def set_observed_properties(self, seed=None, N=1000, max_iterations = 100, threshold=1e-12):
-        if seed is None:
-            seed = np.random.randint(0, 10000)
-        key = jax.random.key(seed)
-        random_normal = np.random.normal(size=len(self.fisher_parameters))
-        cholesky = jnp.linalg.cholesky(self.covariance_at_true)
-        theta_vec_true = jnp.array([self.true_parameters[key] for key in self.fisher_parameters])
-        theta_vec = theta_vec_true + jnp.matmul(cholesky, random_normal)
-        theta_params = self.theta_vec_to_param_dict(theta_vec)
-        self.theta_vectors = []
+    #@partial(jax.jit, static_argnums=(0,))    
+    def update_theta_vec(self, theta_vec, random_normal, theta_vec_true, theta_params_true):
+        theta_params = self.theta_vec_to_param_dict(theta_vec, theta_params_true)
+        theta_params_clipped = bound_theta(theta_params)
+        fisher = self.get_fisher(theta_params_clipped)
+        cholesky_fisher = jnp.linalg.cholesky(fisher)
+        theta_vec_new = theta_vec_true + jax.scipy.linalg.solve_triangular(cholesky_fisher.T, random_normal, lower=False)
         
-        for it in trange(max_iterations):
-            cov = self.get_covariance(theta_params)
+        return theta_vec_new, fisher
+
+    def distance_condition(self, theta_vec, theta_vec_old, fisher, threshold):
+        distance = compute_distance(theta_vec, theta_vec_old, fisher)
+        return distance > threshold
+
+    def iterate_theta_vec(self, theta_vec_true, random_normal, threshold, max_iterations):
+        theta_params_true = self.true_parameters
+        theta_vec, fisher = self.update_theta_vec(theta_vec_true, random_normal, theta_vec_true, theta_params_true)
+        for _ in trange(max_iterations):
+            theta_vec_new, fisher = self.update_theta_vec(theta_vec, random_normal, theta_vec_true, theta_params_true)
+            if jnp.any(jnp.isnan(theta_vec_new)):
+                theta_params = self.theta_vec_to_param_dict(theta_vec, theta_params_true)
+                fish = self.get_fisher(theta_params)
+                cov = self.get_covariance(theta_params)
+                break
+            distance = compute_distance(theta_vec, theta_vec_new, fisher)
+            theta_vec = theta_vec_new
+            if distance < threshold:
+                break
+        print(distance)
+
+        return theta_vec_new
+
+    
+    def iterate_theta_vec_inv(self, theta_vec_true, random_normal, threshold):
+        theta_params = self.theta_vec_to_param_dict(theta_vec_true)
+        fisher = self.get_fisher(theta_params)
+        cov = jnp.linalg.inv(fisher)
+        cholesky_cov = jnp.linalg.cholesky(cov)
+
+        theta_vec_cov = theta_vec_true + jnp.matmul(cholesky_cov, random_normal)
+        theta_vec = theta_vec_cov  
+        for _ in trange(100):
+            theta_params = self.theta_vec_to_param_dict(theta_vec)
+            fisher = self.get_fisher(theta_params)
+            cov = jnp.linalg.inv(fisher)
             cholesky = jnp.linalg.cholesky(cov)
             theta_vec_new = theta_vec_true + jnp.matmul(cholesky, random_normal)
-            theta_params_new = self.theta_vec_to_param_dict(theta_vec_new)
-            self.theta_vectors.append(np.array(theta_vec_new))
-            distance = compute_distance(theta_vec, theta_vec_new, jnp.linalg.inv(cov))
-            if distance < threshold:
-                print(f"Converged after {it} iterations")
-                break
+            distance = compute_distance(theta_vec, theta_vec_new, fisher)
             theta_vec = theta_vec_new
-            theta_params = theta_params_new
-            
+            if distance < threshold:
+                break
+        return theta_vec_new
+    
+    def set_observed_means(self, seed=None, zero_noise=False):
+
         
-        self.observed_covariance = self.get_covariance(theta_params)
-        self.observed_means = theta_vec
+        fisher = self.get_fisher(self.true_parameters)
+        fisher_cholesky = jnp.linalg.cholesky(fisher)
+        
+        if not zero_noise:
+            if seed is None:
+                seed = np.random.randint(0, 10000)
+            key = jax.random.PRNGKey(seed)
+            random_normal = jax.random.normal(key, shape=(len(self.fisher_parameters),))
+            theta_vec_observed = self.theta_vec_true + jax.scipy.linalg.solve_triangular(fisher_cholesky.T, random_normal, lower=False)
+        else:
+            theta_vec_observed = self.theta_vec_true
+        self.observed_means = theta_vec_observed
+        self.observed_parameters = self.theta_vec_to_param_dict(theta_vec_observed, self.true_parameters)
+        self.observed_fisher = fisher
+
+    
+    def set_observed_means_iterate(self, seed=None, max_iterations = 100, threshold=1e-11):
+        if seed is None:
+            seed = np.random.randint(0, 10000)
+        key = jax.random.PRNGKey(seed)
+        random_normal = jax.random.normal(key, shape=(len(self.fisher_parameters),))
+        theta_vec_final=self.iterate_theta_vec(self.theta_vec_true, random_normal, threshold, max_iterations)
+
+        theta_params_final = self.theta_vec_to_param_dict(theta_vec_final, self.true_parameters)
+        theta_params_final_clipped= bound_theta(theta_params_final)
+        self.observed_means = theta_vec_final
+        self.observed_parameters_clipped = theta_params_final_clipped
+        self.observed_parameters = theta_params_final
+        
+    def set_observed_means_inv(self, seed=None, N=1000, max_iterations = 100, threshold=1e-11):
+        if seed is None:
+            seed = np.random.randint(0, 10000)
+        key = jax.random.PRNGKey(seed)
+        random_normal = jax.random.normal(key, shape=(len(self.fisher_parameters),))
+        self.theta_vec_true = jnp.array([self.true_parameters[key] for key in self.fisher_parameters])
+        theta_vec_final=self.iterate_theta_vec_inv(self.theta_vec_true, random_normal, threshold)
+
+        theta_params_final = self.theta_vec_to_param_dict(theta_vec_final, self.theta_vec_true)
+        theta_vec_final_clipped= bound_theta(theta_params_final)
+        theta_params_final_clipped = self.theta_vec_to_param_dict(theta_vec_final_clipped, self.theta_vec_true)
+        self.observed_means = theta_vec_final
+        self.observed_parameters_clipped = theta_params_final_clipped
+        self.observed_parameters = theta_params_final
+        
+    def set_observed_fisher(self):
+        self.observed_fisher = self.get_fisher(self.true_parameters)
+    
+    def set_observed_properties(self, seed=None, N=1000, zero_noise=False):
+        self.set_observed_means(seed=seed, zero_noise=zero_noise)
+        self.set_observed_fisher()
+        self.observed_snr = self.get_snr(self.observed_parameters)
+        
+    def set_observed_properties_inv(self, seed=None, N=1000, max_iterations = 100, threshold=1e-11):
+        self.set_observed_means_inv(seed=seed, N=N, max_iterations=max_iterations, threshold=threshold)
+        self.set_observed_fisher()
         
     def draw_observed_samples(self, seed=None, N=1000):
         if seed is None:
@@ -256,9 +333,10 @@ class FisherSamples(object):
         key = jax.random.key(seed)
         
         observed_samples = {param: [] for param in self.fisher_parameters}
+        print("drawing observed samples")
         
         while len(observed_samples[list(observed_samples.keys())[0]]) < N:
-            samples = draw_samples(mu=self.observed_means, covariance=self.observed_covariance, 
+            samples = draw_samples(mu=self.observed_means, fisher=self.observed_fisher, 
                                    names=self.fisher_parameters,key=key, N=N)
             samples = filter_samples(samples)
             for parameter in observed_samples.keys():
@@ -266,10 +344,11 @@ class FisherSamples(object):
                 
         return {parameter: jnp.array(observed_samples[parameter][:N]) for parameter in observed_samples}
             
-    def draw_physical_samples(self, seed=None, N=1000):
+    def draw_physical_samples(self, seed=None, N=1000, generate=['component_masses', 'chi_eff', 'luminosity_distance', 'cos_iota']):
         
         original_samples = self.draw_observed_samples(seed=seed, N=N)
+        original_samples.update({key: self.true_parameters[key] * jnp.ones(N) for key in self.fixed_argnames})
         
-        physical_samples = convert_to_physical(original_samples, dL0=self.true_parameters['dL0'], rho0=self.true_parameters['rho0'])
+        physical_samples = convert_to_physical(original_samples, generate=generate)
         return physical_samples
     
